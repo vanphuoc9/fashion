94aaa3884825789999a5ae6be36697e5a19266c7
vanphuoc <thaiphuoc1997@gmail.com>
2018-11-09 20:16:28 +0700

    first

--- app/src/main/java/com/example/vanphuoc/fashion/MainActivity.java
+++ app/src/main/java/com/example/vanphuoc/fashion/MainActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    int i = 0;
+    int a =0;
+    int c = 0;
+    int d = 0;
+    int e =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
